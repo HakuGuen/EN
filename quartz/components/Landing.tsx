1272259376e6fd1273b3eed39d99a438149c3388
HakuGuen <hakuguen@gmail.com>
2024-03-16 15:03:11 -0700

    Quartz sync: Mar 16, 2024, 3:03 PM

--- quartz/components/Landing.tsx
+++ quartz/components/Landing.tsx
@@ -5,77 +5,77 @@ import landingStyle from "./styles/Landing.scss"
 export const TOTAL_CARDS = 8
 export const CARDS = {
   "Architecture": (
-    <a href={"./Intro"}>
+    <a href={"/design-o-sphereEN/Intro"}>
       <div class="card card-1">
         <p class="card-title">Archietcture</p>
         <p class="card-subhead"> Global Design </p>
-        <img src="./static/1-ball.png" class="card-illustration-1" />
+        <img src="/design-o-sphereEN/static/1-ball.png" class="card-illustration-1" />
       </div>
     </a>
   ),
   "Landscape-design": (
-    <a href={"./Landscape-Design"}>
+    <a href={"/design-o-sphereEN/Landscape-Design"}>
       <div class="card card-2">
         <p class="card-title">Landscape Design</p>
         <p class="card-subhead">Thiết Kế Cảnh Quan</p>
-        <img src="./static/2-ball.png" class="card-illustration-2" />
+        <img src="/design-o-sphereEN/static/2-ball.png" class="card-illustration-2" />
       </div>
     </a>
   ),
   "Interior-design": (
-    <a href={"./Interior-Design"}>
+    <a href={"/design-o-sphereEN/Interior-Design"}>
       <div class="card card-3">
         <p class="card-title">Interior Design</p>
         <p class="card-subhead">Thiết Kế Nội Thất</p>
-        <img src="./static/3-ball.png" class="card-illustration-3" />
+        <img src="/design-o-sphereEN/static/3-ball.png" class="card-illustration-3" />
       </div>
     </a>
   ),
   "Spatial-design": (
-    <a href={"./Spatial-Design"}>
+    <a href={"/design-o-sphereEN/Spatial-Design"}>
       <div class="card card-4">
         <p class="card-title">Spatial Design</p>
         <p class="card-subhead">Chuyên ngành mới</p>
-        <img src="./static/4-ball.png" class="card-illustration-4" />
+        <img src="/design-o-sphereEN/static/4-ball.png" class="card-illustration-4" />
       </div>
     </a>
   ),
   "Urban-design": (
-    <a href={"./Urban-Design"}>
+    <a href={"/design-o-sphereEN/Urban-Design"}>
       <div class="card card-5">
         <p class="card-title">Urban Design</p>
         <p class="card-subhead">Thiết kế đô thị </p>
-        <img src="./static/5-ball.png" class="card-illustration-5" />
+        <img src="/design-o-sphereEN/static/5-ball.png" class="card-illustration-5" />
       </div>
     </a>
   ),
 
 "Industrial-design": (
-  <a href={"./Industrial-Design"}>
+  <a href={"/design-o-sphereEN/Industrial-Design"}>
     <div class="card card-6">
       <p class="card-title">Industrial Design</p>
       <p class="card-subhead">Đồ Nội Thất</p>
-      <img src="./static/6-ball.png" class="card-illustration-6" />
+      <img src="/design-o-sphereEN/static/6-ball.png" class="card-illustration-6" />
     </div>
   </a>
 ),
 
 "Lighting-design": (
-  <a href={"./Lighting-Design"}>
+  <a href={"/design-o-sphereEN/Lighting-Design"}>
     <div class="card card-7">
       <p class="card-title">Lighting Design</p>
       <p class="card-subhead">ứng dụng</p>
-      <img src="./static/7-ball.png" class="card-illustration-7" />
+      <img src="/design-o-sphereEN/static/7-ball.png" class="card-illustration-7" />
     </div>
   </a>
 ),
 
 "AI-new materials": (
-  <a href={"./AI-New Materials"}>
+  <a href={"/design-o-sphereEN/AI-New Materials"}>
     <div class="card card-8">
       <p class="card-title">AI & New Materials</p>
       <p class="card-subhead">ứng dụng</p>
-      <img src="./static/8-ball.png" class="card-illustration-8" />
+      <img src="/design-o-sphereEN/static/8-ball.png" class="card-illustration-8" />
     </div>
   </a>
 ),
@@ -90,15 +90,15 @@ export default (() => {
           <p class="landing-header">Welcome to Design-O-Sphere</p>
           <p class="page-subhead">
             Started here•{" "}
-            <a href={"./Giới-Thiệu"} target="_blank">
+            <a href={"/design-o-sphereEN/Giới-Thiệu"} target="_blank">
             About
             </a>{" "}
             •{" "}
-            <a href="https://hakuguen.github.io/design-o-sphereVN" target="_blank">
+            <a href="https://hakuguen.github.io/design-o-sphereEN" target="_blank">
             Guide
             </a>{" "}
             •{" "}
-            <a href="https://hakuguen.github.io/design-o-sphereVN" target="_self">
+            <a href="https://hakuguen.github.io/design-o-sphereEN" target="_self">
             Contact
             </a>{" "}
             •{" "}
